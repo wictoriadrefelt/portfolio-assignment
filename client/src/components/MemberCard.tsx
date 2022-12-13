9659b14b1bc2700868bf10e99f3aee0b966c8e02
totaldekadens <angelica.moberg@hotmail.com>
2022-12-13 10:28:32 +0100

    Create member card

---
+++ client/src/components/MemberCard.tsx
@@ -0,0 +1,12 @@
+import { Box, Text } from "@mantine/core";
+import { FC } from "react";
+
+const MemberCard: FC = () => {
+  return (
+    <Box sx={{ width: "100%", maxWidth: 1680, backgroundColor: "whitesmoke" }}>
+      <Text>Membercard</Text>
+    </Box>
+  );
+};
+
+export default MemberCard;
