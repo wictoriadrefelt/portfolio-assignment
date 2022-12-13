4bed8869d824d6a6a40b7fece109d7983eda6b22
totaldekadens <angelica.moberg@hotmail.com>
2022-12-13 10:28:52 +0100

    Membercard

--- client/src/components/MemberCard.tsx
+++ client/src/components/MemberCard.tsx
@@ -1,7 +1,14 @@
 import { Box, Text } from "@mantine/core";
 import { FC } from "react";
 
-const MemberCard: FC = () => {
+type Member = {
+  member: string;
+};
+
+interface Props {
+  member: Member;
+}
+const MemberCard: FC<Props> = (props) => {
   return (
     <Box sx={{ width: "100%", maxWidth: 1680, backgroundColor: "whitesmoke" }}>
       <Text>Membercard</Text>
--- client/src/components/Pages/Members.tsx
+++ client/src/components/Pages/Members.tsx
@@ -1,14 +1,19 @@
-import { Text } from "@mantine/core";
+import { Flex, Text } from "@mantine/core";
 import { FC } from "react";
 import { Link } from "react-router-dom";
+import MemberCard from "../MemberCard";
 
 const Members: FC = () => {
   return (
-    <div>
+    <Flex direction="column">
       <Text>Lista på alla medlemmar</Text>
       {/* Exempel Link. Slug skall vara dynamiskt sedan */}
+      <Flex>
+        <MemberCard member={member} />
+      </Flex>
+
       <Link to={"/members/wictoria"}>Wictoria</Link>
-    </div>
+    </Flex>
   );
 };
 
