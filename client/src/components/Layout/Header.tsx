91b496aaaeac6810c5c4dc13b5d426b219ed9863
wictoriadrefelt <wictoria.soderlunddrefeldt@gmail.com>
2022-12-14 15:42:59 +0100

    header and start for mobile menu

--- client/src/components/Layout/Header.tsx
+++ client/src/components/Layout/Header.tsx
@@ -1,25 +1,112 @@
-import { Box, Header as MantineHeader, Text } from "@mantine/core";
-import { FC } from "react";
+import { useState } from 'react';
+import {
+  Header as MantineHeader,
+  MediaQuery,
+  Burger,
+  useMantineTheme,
+  Image,
+  Box,
+  Flex,
+  Menu,
+  createStyles,
+} from '@mantine/core';
+import { Link } from 'react-router-dom';
 
-const Header: FC = () => {
+const useStyles = createStyles((theme) => ({
+  dropdown: {
+    paddingTop: '100px',
+    display: 'flex',
+    padding: '20px',
+    marginLeft: '0',
+    marginRight: '0',
+    postion: 'relative',
+    height: '150px',
+    background: 'white',
+    justifyContent: 'center',
+    marginTop: '40px',
+    width: '400px',
+    border: '1 px solid black',
+  },
+  menuLinks: {
+    textDecoration: 'none',
+    fontFamily: 'Quicksand',
+    alignItems: 'flex-end',
+    color: 'black',
+    width: '80%',
+  }
+}))
+
+
+export default function AppShellDemo() {
+  const theme = useMantineTheme();
+  const [opened, setOpened] = useState(false);
+  const { classes } = useStyles();
+
+  const link = {
+    fontSize: 14,
+    color: '#FFFFFF',
+    padding: 3,
+    paddingRight: 15,
+    FontFace: 'Quicksand',
+    textDecoration: 'none',
+
+}
+
   return (
-    <MantineHeader
-      p={16}
-      withBorder={false}
-      height={60}
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        backgroundColor: "whitesmoke",
-      }}
-    >
-      <Box
-        sx={{ width: "100%", maxWidth: 1680, backgroundColor: "whitesmoke" }}
-      >
-        <Text>Header</Text>
-      </Box>
-    </MantineHeader>
+        <MantineHeader p={10}
+          withBorder={false}
+          height={80}
+          sx={{
+            display: "flex",
+            backgroundColor: theme.colors.brand[3],
+          }}
+        >
+        <Box
+          sx={{ width: '100%', maxWidth: 1680 }} > 
+          <a href="/" >
+        <Image
+          width={200}
+          height={60}
+          src="..\src\assets\logo.png"
+        />
+          </a>
+        </Box>
+        <MediaQuery smallerThan="xs" styles={{ display: 'none' }}>
+          <Flex direction='row' p='sm'>
+          <Link hidden={opened} style={link}
+            to="/login">Login </Link>
+          <Link style={link} hidden={opened} to='/contact'>Contact</Link>     
+          </Flex>      
+        </MediaQuery> 
+          <Flex direction='column' >
+        <MediaQuery largerThan='xs' styles={{ display: 'none'}}>
+            <Flex> 
+              <Menu shadow='md' width='80%'>
+                  <Menu.Target>
+                <Burger
+                    opened={opened}
+                    onClick={() => setOpened((o) => !o)}
+                    size="sm"
+                    color={theme.colors.brand[0]}
+                    mr="xl"
+                    pb='sm'
+                />
+                  </Menu.Target>
+                <Menu.Dropdown className={classes.dropdown}>
+                  <Link to='/'> 
+                <Menu.Item component="a" className={classes.menuLinks}>Home</Menu.Item>
+                  </Link>
+                  <Link to='/login'> 
+                <Menu.Item component="a" className={classes.menuLinks}>Login</Menu.Item>
+                  </Link>
+                  <Link to='/contact'> 
+                <Menu.Item component='a' className={classes.menuLinks}>Contact</Menu.Item>
+                  </Link>
+                </Menu.Dropdown>
+                </Menu>
+                </Flex>
+                  </MediaQuery>
+              </Flex>
+          </MantineHeader>
   );
-};
-
-export default Header;
+}
