4c802f8563d424d8cfa905450e893c12cbc189be
Bami85 <91563447+Bami85@users.noreply.github.com>
2022-12-14 10:48:54 +0100

    footer-done

--- client/src/components/Layout/Footer.tsx
+++ client/src/components/Layout/Footer.tsx
@@ -1,25 +1,62 @@
-import { Box, Footer as MantineFooter, Text } from "@mantine/core";
-import { FC } from "react";
+import { createStyles, Container, Group,Text } from '@mantine/core';
+import { Button } from '@mantine/core';
 
-const Footer: FC = () => {
+const useStyles = createStyles((theme) => ({
+  footer: {
+    backgroundColor: "#EAE6E3",
+    marginTop: 120,
+    borderTop: `1px solid ${
+      theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.green[2]
+    }`,
+  },
+ 
+
+  inner: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingTop: theme.spacing.xl,
+    paddingBottom: theme.spacing.xl,
+
+    [theme.fn.smallerThan('xs')]: {
+      flexDirection: 'column',
+    },
+  },
+  button: {
+    backgroundColor:"#7F9593",
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingTop: theme.spacing.xl,
+    paddingBottom: theme.spacing.xl,
+
+    [theme.fn.smallerThan('xs')]: {
+      flexDirection: 'column',
+    },
+  },
+
+
+  links: {
+    [theme.fn.smallerThan('xs')]: {
+      marginTop: theme.spacing.md,
+    },
+  },
+}));
+
+export default function Footer() {
+  const { classes } = useStyles();
+
   return (
-    <MantineFooter
-      p={16}
-      withBorder={false}
-      height={"auto"}
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        backgroundColor: "whitesmoke",
-      }}
-    >
-      <Box
-        sx={{ width: "100%", maxWidth: 1680, backgroundColor: "whitesmoke" }}
-      >
-        <Text>Footer</Text>
-      </Box>
-    </MantineFooter>
+    <div className={classes.footer}>
+      <Container className={classes.inner}>
+        <Text> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore </Text>
+          <Button color="teal">Login</Button> 
+          <Button color="teal">Contact us </Button>
+      </Container>
+      
+    </div>
   );
-};
+}
 
-export default Footer;
+
+
