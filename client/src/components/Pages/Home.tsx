7cb61bd1b1d8cdd2e498611bd6fcf79add364a81
TingCythia <91254839+TingCythia@users.noreply.github.com>
2022-12-16 14:42:38 +0100

    Startsidan uppdaterad

--- client/src/components/Pages/Home.tsx
+++ client/src/components/Pages/Home.tsx
@@ -7,48 +7,62 @@ const Home: FC = () => {
     <>
       <Image 
       src="./src/assets/people_1920.jpg"
-      height="100vh"
+      height="107vh"
       width="100vw"
-      sx={{position:"fixed", top:0, left:0}}
+      sx={{position:"absolute", top:80, left:0}}
       />
       <Box 
       sx=
-      {{
-      position:"fixed",  
+      {(theme)=>({
+      position:"sticky",  
       left:0,    
-      top:60,
-      bottom:55,
-      zIndex:9999,
+      top:0,
+      bottom:0,
+      right:0,
+      zIndex:1,
       width:"100vw",
+      height:"100vh",
+      marginTop:"-17px",
+      marginLeft:"-50px",
+      marginBottom:"-120px",
+      paddingLeft:"40px",
       background: 
-      "linear-gradient(140deg, #FFFFFF 20%, rgba(255, 255, 255, 0) 75%)",
-      }}
+      "linear-gradient(160deg, #FFFFFF 20%, rgba(255, 255, 255, 0) 75%)",
+      })}
       >
       <Title 
       sx={(theme) => ({
         color: theme.colors.brand[3],
         fontSize : theme.fontSizes.xl,
         margin: theme.spacing.md,
-        paddingTop: 50,
+        paddingTop: 70,
         letterSpacing:-2,
         wordSpacing:-2,
         })}>
-      Sed ut perspiciatis unde <br /> omnis
+      Vi lyfter er organisation till  <br /> nästa nivå
       </Title>
-      <Text 
+      <Title
       sx={(theme) => ({
         color: theme.colors.brand[4],
         fontSize : theme.fontSizes.lg,
         marginLeft: theme.spacing.md,
-        maxWidth: 550,
-        textAlign:"justify",
+        maxWidth: 650,
+        textAlign:"left",
         paddingRight:"25px",
         paddingTop: 40,
         paddingBottom:40,
+        [theme.fn.smallerThan(850)]:{
+        paddingTop: 10,
+        paddingBottom:10,
+        }
         })}
+        order={2}
         >
-        Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem.
-      </Text>
+        Våra konsulter står redo, med relevant erfarenhet för att möta era
+        nuvarande utmaningar – oavsett om det gäller den dagliga verksamheten,
+        en förändringsresa eller utveckling. Med stor passion för matchning
+        och utveckling, säkerställer vi rätt konsult för era behov
+      </Title>
       <Link to="/members">
       <Button
       sx={(theme) => ({
@@ -64,7 +78,7 @@ const Home: FC = () => {
         width: 192,
         })}
         
-      >Members</Button>
+      >Vårt Team</Button>
       </Link>
       </Box>
     </>
