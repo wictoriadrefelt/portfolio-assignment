76e4fb02ec2efc997e729829120879a6a5d2101f
wictoriadrefelt <wictoria.soderlunddrefeldt@gmail.com>
2022-12-16 12:24:14 +0100

    added responsive design to contact page

--- client/src/components/Pages/Contact.tsx
+++ client/src/components/Pages/Contact.tsx
@@ -1,104 +1,109 @@
 import { Text, Title } from "@mantine/core";
 import { FC } from "react";
 import { useForm } from '@mantine/form';
-import { TextInput, Button, Textarea, Flex, Box } from '@mantine/core';
+import { TextInput, Button, Textarea, Box, Flex } from '@mantine/core';
 import { Image } from '@mantine/core';
-import { NavLink } from '@mantine/core';
-import { CSSObject } from "@emotion/react";
-import {IconAt} from '@tabler/icons';
-
- 
-
- 
-
-
-
+import { IconMail, IconPhone } from "@tabler/icons";
+import { Link } from "react-router-dom";
 
 const Contact: FC = () => {
-  
-    
-  
-  
   const form = useForm({
-    
-    initialValues: { name: '', email: '', subject: '', Textarea: '' },
+    initialValues: { name: '', email: '', subject: '', textAre: '' },
 
     // functions will be used to validate values at corresponding key
     validate: {
       name: (value) => (value.length < 2 ? 'Name must have at least 2 letters' : null),
       email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
       subject: (value) => (value.length < 2 ? 'Subject must have at least 2 letters' : null),
-      Textarea: (value) => (value.length < 2 ? 'Textarea must have at least 2 letters' : null),
+      textAre: (value) => (value.length < 2 ? 'Textarea must have at least 2 letters' : null),
     },
   });
 
   return (
-    
-    <Box
-    bg="brnad.2"
-    sx={{ width: '1600px' , }}
-    m="xl"
-    >
-   
-   <IconAt/>
-    <form onSubmit={form.onSubmit(console.log)}>
-  
-      
-      <Title order={1} sx={{ fontSize: '3em', textAlign: 'center'}} >Kontakt</Title>
-      <Title order={2} sx={{ margin:'50px'}}>Kontakta Oss</Title>
-      
-   
-      
-      <Title order={2} sx={{ textAlign:'right', marginRight:'260px', marginTop: '-20px'}}>Kontoret</Title>
-      
-      <Image src="./src/assets/map.png" style={{ width: 600, float: 'right', marginTop: '20px'}} />
-      
-      
-      
-      <Text style={{  marginTop: '380px', marginLeft: '1000px'}} ><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-phone" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-        <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2"></path>
-      </svg> +4673-712 34 56</Text>
+    <>
+      <Title sx={{ fontSize: '3em', textAlign: 'center', margin: '10px' }} >Kontakt</Title>
+      <Flex sx={(theme) => ({
+        marginTop: '80px',
+        flexWrap: 'wrap',
+        justifyContent: 'space-evenly',
+        [theme.fn.smallerThan('md')]: {
+          marginTop: "0px",
+          wrap: 'wrap',
+        },
+      })}>
+        <Box>
+          <form onSubmit={form.onSubmit(console.log)}>
+            <Title order={2} mt='xl' mb='md' sx={(theme) => ({
+              [theme.fn.smallerThan("sm")]: {
 
+              },
+              [theme.fn.smallerThan("xs")]: {
 
-      <Text style={{  marginTop: '10px', marginLeft: '1000px'}}>
-        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-mail" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-        <rect x="3" y="5" width="18" height="14" rx="2"></rect>
-        <polyline points="3 7 12 13 21 7"></polyline>
-      </svg> support@brainwave.se</Text>
-    
-    
-      <TextInput label="Namn" placeholder="Namn" {...form.getInputProps('name')}
-       sx={{ maxWidth: 500, marginTop: '-450px'}} />
-      <TextInput mt="md" label="Email" placeholder="Email" {...form.getInputProps('email')} 
-      sx={{  maxWidth: 500,}}/>
-      <TextInput mt="md" label="Ämne" placeholder="Ämne" {...form.getInputProps('subject')}
-      sx={{ maxWidth: 500,}} />
-      
-      <Textarea placeholder="Din meddelande" label="Din meddelande" size="md" withAsterisk 
-      sx={{ maxWidth: 500,}} />
-      <Button type="submit" mt="md"
-      sx={(theme) => ({
-        backgroundColor: theme.colors.brand[3],
-    })}>
-        Kontakta Oss
-      </Button>
-    
-    
-      
+              },
+            })}>Kontakta Oss</Title>
 
-    </form>
-    
-    </Box>
+            <TextInput label="Namn" placeholder="Namn" {...form.getInputProps('name')}
+              sx={{ maxWidth: 500 }} />
+            <TextInput mt="md" label="Email" placeholder="Email" {...form.getInputProps('email')}
+              sx={{ maxWidth: 500, }} />
+            <TextInput mt="md" label="Ämne" placeholder="Ämne" {...form.getInputProps('subject')}
+              sx={{ maxWidth: 500, }} />
+
+            <Textarea placeholder="Ditt meddelande" label="Ditt meddelande" mt='md' size='sm' withAsterisk
+              sx={{ maxWidth: 500, }} />
+            <Button type="submit" mt="md" sx={(theme) => ({ backgroundColor: theme.colors.brand[3] })}>
+              Kontakta Oss
+            </Button>
+          </form>
+        </Box>
+        <Box mt='25px' >
+          <Title order={2} mt='xs' sx={(theme) => ({
+            [theme.fn.smallerThan('md')]: {
+              marginLeft: '210px'
+            },
+            [theme.fn.smallerThan("sm")]: {
+              marginLeft: '210px'
+            },
+          })}>Kontoret</Title>
+          <a target={"_blank"} 
+            href={"https://www.google.com/maps/place/Medieinstitutet/@57.7097478,11.9924801,17.09z/data=!4m5!3m4!1s0x464ff30de9cbee3d:0xbb3f902b63916df6!8m2!3d57.709754!4d11.9945328"}> <Image src="./src/assets/map.png" style={{ width: 600, marginTop: '40px' }} sx={(theme) => ({
+            [theme.fn.smallerThan("md")]: {
+              width: '300px'
+            },
+            [theme.fn.smallerThan("xs")]: {
+              width: '150px'
+            },
+          })} /> </a>
+
+          <Box sx={(theme) => ({
+            [theme.fn.smallerThan("sm")]: {
+              marginLeft: '170px'
+            },
+            [theme.fn.smallerThan("xs")]: {
+              marginLeft: '170px'
+            },
+          })} >
+            <Link
+              to="#"
+              onClick={(e) => {
+                window.location.href = "mailto:support@brainwave.se";
+                e.preventDefault();
+              }}
+            >
+              <Flex mt='xs' direction='column' >
+                  <Text style={{ color: 'black' }}>
+                  <IconMail style={{ color: 'black', marginRight: '4px', marginTop: '15px',}} /> support@brainwave.se </Text>
+                  <Text >
+                    <IconPhone style={{marginTop: '15px', marginRight: '4px' }} />+4673-712 34 56
+                      </Text>
+              </Flex>
+            </Link>
+          </Box>
+        </Box>
+      </Flex >
+    </>
   );
 }
-
- 
-const styleObject = (): CSSObject => ({
-  
-});
-
 
 export default Contact;
 
