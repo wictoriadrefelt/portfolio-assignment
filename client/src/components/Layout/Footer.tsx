54ce3823831fcaf58d5265973023a8eb301c41d1
Bami85 <91563447+Bami85@users.noreply.github.com>
2022-12-16 10:32:47 +0100

    footer-review

--- client/src/components/Layout/Footer.tsx
+++ client/src/components/Layout/Footer.tsx
@@ -1,28 +1,32 @@
+import { Flex } from '@mantine/core';
 import { createStyles, Container, Group,Text } from '@mantine/core';
 import { Button } from '@mantine/core';
+
+import { Link } from "react-router-dom";
 
 const useStyles = createStyles((theme) => ({
   footer: {
     backgroundColor: "#EAE6E3",
     marginTop: 120,
-    borderTop: `1px solid ${
-      theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.green[2]
-    }`,
+   
   },
  
 
   inner: {
-    display: 'flex',
+    display: 'flex', 
+    flexWrap:"wrap",
     justifyContent: 'space-between',
     alignItems: 'center',
     paddingTop: theme.spacing.xl,
     paddingBottom: theme.spacing.xl,
-
-    [theme.fn.smallerThan('xs')]: {
+    [theme.fn.smallerThan('md')]: {
       flexDirection: 'column',
-    },
+    }
+
+    
   },
   button: {
+    flexWrap:'wrap',
     backgroundColor:"#7F9593",
     display: 'flex',
     justifyContent: 'space-between',
@@ -30,14 +34,14 @@ const useStyles = createStyles((theme) => ({
     paddingTop: theme.spacing.xl,
     paddingBottom: theme.spacing.xl,
 
-    [theme.fn.smallerThan('xs')]: {
+    [theme.fn.smallerThan('sm')]: {
       flexDirection: 'column',
     },
   },
 
 
   links: {
-    [theme.fn.smallerThan('xs')]: {
+    [theme.fn.smallerThan('sm')]: {
       marginTop: theme.spacing.md,
     },
   },
@@ -49,14 +53,38 @@ export default function Footer() {
   return (
     <div className={classes.footer}>
       <Container className={classes.inner}>
-        <Text> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore </Text>
-          <Button color="teal">Login</Button> 
-          <Button color="teal">Contact us </Button>
+        <Text sx= {{paddingBottom:"20px"}}> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore </Text>
+        <Flex sx={{width:"200px" ,justifyContent:"space-around"}}> 
+        <Link to ="/Login>">
+          <Button color="brand.3">Login</Button> 
+        </Link>
+        <Link to ="/Contact>">
+          <Button color="brand.3">Contact us </Button>
+        </Link>
+        </Flex>  
+      </Container>
+      <Container>
+      
+        <Text sx ={{paddingBottom:"30px"}} color="EAE6E3" size="md" align="center" >
+          <Text> Anders Personsgatan 18, Göteborg </Text>
+          © 2022 Brainwave. All rights reserved.
+        </Text >
+         
       </Container>
       
     </div>
+    
   );
 }
+
+
+
+
+
+
+
+
+
 
 
 
